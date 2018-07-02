2cf56217e997d0f0ecb5576db7b15381bd45a772
knorjador <headers@protonmail.com>
2018-07-02 09:59:48 +0100

    navbar

--- app/controllers/YoutubeController.php
+++ app/controllers/YoutubeController.php
@@ -14,22 +14,27 @@ class YoutubeController extends Controller {
   public function render()
   {
 
+    echo $this->twig->render('news/youtube.html.twig', [
+      'videos' => $this->getVideos('bitcoin%20fr')
+    ]);
+
+  }
+
+  /**
+   * GetVideos method
+   *
+   * @return array $videos
+   */
+  private function getVideos($query)
+  {
+
     $base_url   = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
-    $query      = 'bitcoin%20fr';
     $maxResults =  10;
     $order      = 'date';
     $langage    = 'fr';
     $key        = 'AIzaSyAV9eb5jfZUHg0XPuwzo2zRS_Kwimlz4mg';
 
     $url = $base_url.'&q='.$query.'&maxResults='.$maxResults.'&regionCode=FR&relevanceLanguage='.$langage.'&order='.$order.'&safeSearch=none&key='.$key;
-
-    // $url = $base_url.'&q='.$query.'&maxResults='.$maxResults.'&regionCode=FR&order='.$order.'&safeSearch=none&key='.$key;
-
-    // $url = $base_url.'&q='.$query.'&maxResults='.$maxResults.'&relevanceLanguage='.$langage.'&order='.$order.'&safeSearch=none&key='.$key;
-
-    // $url    = $base_url.'&q='.$query.'&location=46.72574,2.990717&locationRadius=300km&type=video&key='.$key;
-
-    // var_dump($url); die();
 
     $data   = json_decode(file_get_contents($url), true)['items'];
     $videos = [];
@@ -54,9 +59,8 @@ class YoutubeController extends Controller {
     // echo '</pre>';
     // die();
 
-    echo $this->twig->render('news/youtube.html.twig', [
-      'videos' => $videos
-    ]);
+    return $videos;
+
   }
 
 }
