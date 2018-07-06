3d541dcf5b233e2c59f329f92239f5d6d0ebcb6d
rasha84 <rasha.slman84@gmail.com>
2018-07-06 13:30:56 +0200

    localbitcoin on home

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,116 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 }).addTo(map);
 
 
+const eventBitcoin = function() {
+
+  const bitcoins = document.querySelectorAll('.bitcoin')
+
+  bitcoins.forEach(bitcoin => {
+
+    bitcoin.addEventListener('click', e => {
+
+      e.preventDefault()
+      // console.log(bitcoin.value);
+
+      post(bitcoin.value)
+
+    })
+
+  })
+
+}()
+
+
+/**** **** **** **** **** **** **** ****
+ > POST
+**** **** **** **** **** **** **** ****/
+
+const post = data => {
+
+  const XHR = new XMLHttpRequest()
+
+  XHR.open('POST', `${window.location.href}`)
+
+  XHR.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+
+  XHR.send(`bitcoin=${data}`)
+
+  XHR.onreadystatechange = () => {
+  
+    if(XHR.readyState === 4 && XHR.status === 200) {
+
+      let kind = JSON.parse(XHR.responseText).kind;
+      
+      switch (kind){
+
+        case "cash" : 
+          let parsed = JSON.parse(XHR.responseText).data;
+    
+          parsed.forEach(element => {
+    
+          let lat = element.data.lat;
+    
+          let lon = element.data.lon;
+    
+          let username = element.data.profile.username;
+            
+          let price = element.data.temp_price;
+    
+          //if we need the min amount or max amount
+    
+          // let min = element.data.min_amount;
+          
+          // let max = element.data.max_amount;
+            
+          let url = element.actions.public_view; 
+            
+          addMarker(lat, lon);
+    
+          let marker = addMarker(lat, lon);
+
+          marker.bindPopup(username + '<br>'+ price +'<br>' +'<a href='+url+' onclick="window.open(this.href); return false;">'+url +' </a>' ).openPopup();
+    
+          })
+          break;
+
+        case "atm" : 
+
+          //////
+          break;
+
+        case "shop" : 
+
+          /////
+          break;
+
+
+        case "reset" : 
+
+          /////
+          break;
+
+        default:
+
+
+      }
+
+
+    }
+
+  }
+
+
+}
+
+function addMarker(lat, lon){
+
+  let myIcon = L.icon({
+    iconUrl: 'assets/img/icon.png',
+    iconSize: [38, 50],
+    iconAnchor: [22, 50],
+    popupAnchor: [-3, -76]})
+ 
+    let marker = L.marker([lat, lon],{icon: myIcon}).addTo(map);
+
+     return marker;
+}
